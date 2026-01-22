7ffd3a20b79a3aa903da4375ec8fcfa18058138a
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-22 13:56:17 +0000

    resetbttn

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const cookieButton=document.querySelector("#cookieButton");
 const cookieCountSpan=document.querySelector("#cookieCount");
 const cpsSpan=document.querySelector("#cps");
 
+
 let cookies = 0;
-let cps = 0;
+let cps = 1;
 
 
 cookieCountSpan.textContent=cookies;
@@ -14,9 +15,28 @@ cpsSpan.textContent=cps;
 cookieButton.addEventListener("click",()=>{
     cookies++;
     cookieCountSpan.textContent= cookies;
+    saveGame();
 });
 
 setInterval (()=>{
       cookies = cookies + cps;
       cookieCountSpan.textContent = cookies;
+      saveGame();
 },1000);
+
+function saveGame(){
+    localStorage.setItem("cookies", cookies);
+    localStorage.setItem("cps",cps);
+};
+
+const savedCookies = localStorage.getItem("cookies");
+const savedCps = localStorage.getItem("cps");
+
+cookies = Number(savedCookies) || 0;
+cps = Number(savedCps) || cps;
+
+cookieCountSpan.textContent = cookies;
+cpsSpan.textContent = cps;
+
+
+
