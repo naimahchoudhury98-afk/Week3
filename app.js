ece83eb4e69da9bc71eeb8a3b5c2c8e6c9fbdf45
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-22 14:04:48 +0000

    shop

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("Just testing if this is working")
 const cookieButton=document.querySelector("#cookieButton");
 const cookieCountSpan=document.querySelector("#cookieCount");
 const cpsSpan=document.querySelector("#cps");
+const resetButton=document.querySelector("#resetButton");
 
 
 let cookies = 0;
@@ -38,5 +39,13 @@ cps = Number(savedCps) || cps;
 cookieCountSpan.textContent = cookies;
 cpsSpan.textContent = cps;
 
+resetButton.addEventListener("click", ()=>{
+    cookies =0;
+    cps= 1;
 
+    cookieCountSpan.textContent=cookies;
+    cpsSpan.textContent=cps;
+    localStorage.removeItem("cookies");
+    localStorage.removeItem("cps");
+})
 
