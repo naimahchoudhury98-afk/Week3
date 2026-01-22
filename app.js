8b8701b80aed4c46cd7664156e1f5fda4ab51d4c
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-22 11:30:38 +0000

    intervaladding

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ console.log("Just testing if this is working")
 
 const cookieButton=document.querySelector("#cookieButton");
 const cookieCountSpan=document.querySelector("#cookieCount");
+const cpsSpan=document.querySelector("#cps");
 
 let cookies = 0;
+let cps = 0;
+
 
 cookieCountSpan.textContent=cookies;
+cpsSpan.textContent=cps;
+
+cookieButton.addEventListener("click",()=>{
+    cookies++;
+    cookieCountSpan.textContent= cookies;
+});
+
