434872066cb39f9f51f3392fa7ca3d0fbfab3020
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-01-22 10:09:36 +0000

    makingbuttonwork

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Just testing if this is working")
+
+const cookieButton=document.querySelector("#cookieButton");
+const cookieCountSpan=document.querySelector("#cookieCount");
+
+let cookies = 0;
+
+cookieCountSpan.textContent=cookies;
